5778af53990b9f08ad994ecedbef8714f4554fdd
Paul Keldsen <Pkeld148@gmail.com>
2021-06-29 13:38:08 -0400

    Added event listener to Skills button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello World!");
+
+var skillButton = document.getElementById("skill-button");
+
+skillButton.addEventListener("click", skillButtonPress);
+
+function skillButtonPress() {
+  console.log("You pressed the Skills button!");
+}
